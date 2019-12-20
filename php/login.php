dba6ce41c3cd2c3da5e38beaec5444d424b65e39
7ahara <48213380+7ahara@users.noreply.github.com>
2019-12-20 17:11:04 +0200

    pered slaiderom

--- php/login.php
+++ php/login.php
@@ -79,39 +79,7 @@
         </form>
     </div>
     <div class="conteiner">
-    <h4>Хочешь порисовать?(смотри вниз)</h4>
-    <canvas id = "canvas"  class="formochka">Обновите браузер</canvas>
-		<script>
-			var 
-			  canv =  document.getElementById('canvas'),
-			  ctx = canv.getContext('2d');
-
-			  canv.width = window.innerWidth;
-			  canv.height = window.innerHeight;
-
-			  canv.addEventListener('mousedown',function(){
-				  isMouseDown = true;
-			  });
-			  
-			  canv.addEventListener('mouseup',function(){
-				  isMouseDown = false;
-				  ctx.beginPath();
-			  });
-			  ctx.lineWidth = 4 * 2;
-			  canv.addEventListener('mousemove',function(e){
-				  if (isMouseDown){
-					  ctx.lineTo(e.clientX, e.clientY);
-					  ctx.stroke();
-
-					  ctx.beginPath();
-					  ctx.arc(e.clientX, e.clientY, 4,0, Math.PI * 2)
-					  ctx.fill();
-
-					  ctx.beginPath();
-					  ctx.moveTo(e.clientX, e.clientY);
-				  }
-			  });
-        </script>
+    <h4><a href="../canv.html">Хочешь порисовать?</a></h4>
     </div>
     <footer>
         <div class="site__bottom">
